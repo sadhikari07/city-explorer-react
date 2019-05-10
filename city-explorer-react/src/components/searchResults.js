c2e34f8184b776378f1f74a76954517687241163
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-05-10 09:59:20 -0700

    city explorer front-end complete

--- city-explorer-react/src/components/searchResults.js
+++ city-explorer-react/src/components/searchResults.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import superagent from 'superagent';
+import { Link } from 'react-router-dom';
 require('dotenv').config();
+
 
 class Yelp extends React.Component{
 constructor(props){
@@ -26,8 +28,12 @@ yelpFunction = async () => {
 render(){
   return(
     <section>
-      <ul>
-         {this.state.yelp.length>0 ? (this.state.yelp.map(element => <li> <p>{element.name}</p> <p>{element.price}</p> <p>{element.rating}</p> <p><img src = {element.image_url}/></p> </li>)) : 'nothing'}
+      <ul className = 'yelpResult'>
+          <h3 className = 'resultTitle'>Results from the Yelp API</h3>
+         {this.state.yelp.length>0 ? (this.state.yelp.map(element => <li> 
+         <p>{element.name}</p> <a href = {element.url}></a>
+          <p>The average rating is {element.rating} out of 5 and the average cost is {element.price} out of 4</p>
+          <p><img className = 'yelpImage' src = {element.image_url}/></p> </li>)) : 'nothing'}
       </ul>
     </section>
   );
@@ -50,7 +56,6 @@ class MovieDB extends React.Component{
   movieFunction = async () => {
     let url = `https://city-explorer-backend.herokuapp.com/movies`;
     let movieData = await superagent.get(url).query({data: this.props.location});
-    // console.log('movies', movieData);
     this.setState({
       movies: movieData.body
     });  
@@ -58,11 +63,11 @@ class MovieDB extends React.Component{
   }
 
   render(){
-    // console.log('movies in render', this.state.movie[0]);
     return(
       <section>
-        <ul>
-           {this.state.movies.length>0 ? (this.state.movies.map(element => <li><p> {element.title}</p> <p>{element.overview}</p></li>)) : 'nothing'}
+        <ul className = 'movieResult'>
+        <h3 className = 'resultTitle'>Results from the Movies API</h3>
+           {this.state.movies.length>0 ? (this.state.movies.map(element => <li><p><span className = 'movieTitleOnDisplay'> {element.title}</span> was released on {element.released_on}</p> <p>{element.overview}</p></li>)) : 'nothing'}
         </ul>
       </section>
     );
@@ -98,8 +103,10 @@ class MovieDB extends React.Component{
     render(){
       return(
         <section>
-          <ul>
-             {this.state.weather[0] ? (this.state.weather.map(element => <li> <p>{element.forecast}</p> <p>{element.time}</p></li>)) : 'nothing'}
+          <ul className = 'weatherResult'>
+          <h3 className = 'resultTitle'>Results from the Dark Sky API</h3>
+             {this.state.weather[0] ? (this.state.weather.map(element => <li>
+              <p>The forecast for {element.time} is: {element.forecast}</p> </li>)) : 'nothing'}
           </ul>
         </section>
       );
@@ -112,10 +119,9 @@ class MovieDB extends React.Component{
     render(){
       return(
         <section>
-           <ul>
-        <li>
-          EventBrite API result
-        </li>
+        <ul className = 'eventResult'>
+        <h3 className = 'resultTitle'>Results from the EventBrite API</h3>
+        <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p> <p><li>EventBrite API result</li></p>
       </ul>
         </section>
       );
@@ -128,37 +134,47 @@ class SearchResult extends React.Component{
 
     return(
       <section>
-      <p>here is your result</p>
 
-      {this.props.location ? 
-      <Yelp
-      location = {this.props.location}
-      />
-      : null 
-    }
+        {this.props.location ? 
+          <h2></h2>
+          : <p></p> 
+        }
 
-      {this.props.location ? 
-        <MovieDB 
-        location = {this.props.location}
-        latitude = {this.props.latitude}
-        longitude = {this.props.longitude}
-        />
-      : null 
-      }
+        {this.props.location ? 
+          <DarkSky 
+            location = {this.props.location}
+            searchQuery = {this.props.searchQuery}
+            latitude = {this.props.latitude}
+            longitude = {this.props.longitude}
+            /> 
+        : null 
+        }
 
-
-
-      {this.props.location ? 
-        <DarkSky 
+        {this.props.location ? 
+          <Yelp
           location = {this.props.location}
-          searchQuery = {this.props.searchQuery}
+          />
+          : null 
+        }
+
+        {this.props.location ? 
+          <MovieDB 
+          location = {this.props.location}
           latitude = {this.props.latitude}
           longitude = {this.props.longitude}
-          /> 
-      : null 
-    }
+          />
+        : null 
+        }
 
-      <EventBrite/>
+
+
+      
+
+      {this.props.location ? 
+        <EventBrite/>
+
+        : null 
+      }
 
       </section>
     );
